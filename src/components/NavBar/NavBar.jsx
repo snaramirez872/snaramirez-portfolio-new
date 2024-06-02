011b64dbd5694792c6225ae48b24700cb4204938
snaramirez872 <seanramirez818@gmail.com>
2024-06-02 00:28:40 -0400

    implemented and designed nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,13 @@ import "./NavBar.css";
 export default function NavBar() {
     return (
         <nav className="NavBar">
-            <Link to="/" className="logo">Sean A Ramirez</Link>
-            <div className='links'>
-                <Link to="/" className='nav-link'>About Me</Link>
-                <Link to="/projects" className='nav-link'>Projects</Link>
-                <a href={import.meta.env.VITE_resumeLink} className='resume-link' target='_blank' rel='noreferrer'>Resume</a>
+            <div className='nav-container'>
+                <Link to="/" className="logo">Sean A Ramirez</Link>
+                <div className='links'>
+                    <Link to="/" className='nav-link'>About Me</Link>
+                    <Link to="/projects" className='nav-link'>Projects</Link>
+                    <Link to={import.meta.env.VITE_resumeLink} className='resume-link' target='_blank' rel='noreferrer'>Resume</Link>
+                </div>
             </div>
         </nav>
     );
