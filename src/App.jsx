9067f74ce5c2d93ed0778c19daf57c7c36bfa9e3
snaramirez872 <seanramirez818@gmail.com>
2024-06-01 23:51:16 -0400

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
 import NavBar from './components/NavBar/NavBar';
+import Projects from './pages/Projects/Projects';
+import AboutMe from './pages/AboutMe/AboutMe';
+import './App.css'
 
 export default function App() {
   return (
@@ -10,7 +12,10 @@ export default function App() {
       <div className="App">
         <NavBar />
         <div className="pages">
-          Hello
+          <Routes>
+            <Route path='/' element={<AboutMe />} />
+            <Route path='/projects' element={<Projects />} />
+          </Routes>
         </div>
       </div>
     </Router>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <nav className="NavBar">
             <Link to="/" className="logo">Sean A Ramirez</Link>
-            <div className='mr-[7vh] space-x-11 text-xl hidden md:flex'>
+            <div className='links'>
                 <Link to="/" className='nav-link'>About Me</Link>
                 <Link to="/projects" className='nav-link'>Projects</Link>
                 <a href={import.meta.env.VITE_resumeLink} className='resume-link' target='_blank' rel='noreferrer'>Resume</a>
